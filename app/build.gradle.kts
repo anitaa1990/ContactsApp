bbf7a4a4ad5719a0ec4519808875f64cf62ca6cd
Anitaa Murthy <murthyanitaa@gmail.com>
2024-06-11 08:39:41 +0530

    Added Lifecycle with compose lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,8 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
 
+    implementation(libs.androidx.lifecycle.runtime.compose.android)
+
     // Hilt
     implementation(libs.hilt.android)
     implementation(libs.hilt.navigation.compose)
