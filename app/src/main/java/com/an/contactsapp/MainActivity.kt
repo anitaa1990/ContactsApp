c459ebc4475b3d55e4feab0452f8ad1a34acbe17
Anitaa Murthy <murthyanitaa@gmail.com>
2025-01-07 17:16:05 +0530

    Added theme to the app

--- app/src/main/java/com/an/contactsapp/MainActivity.kt
+++ app/src/main/java/com/an/contactsapp/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.an.contactsapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.an.contactsapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,7 +11,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
