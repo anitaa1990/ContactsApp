144e8d8c46b77ddbe542e1d0c2830cb904d8ebf2
Anitaa Murthy <murthyanitaa@gmail.com>
2025-01-07 16:23:16 +0530

    Added MainActivity UI

--- app/src/main/java/com/an/contactsapp/MainActivity.kt
+++ app/src/main/java/com/an/contactsapp/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.an.contactsapp
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.an.contactsapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
